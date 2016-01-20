3e67ebef41002fc9fd41b310c8289f753a7456e8
BlackKnight7 <blackknight7.h@gmail.com>
2016-01-20 19:18:42 +0800

    Create staticlist demo.

---
+++ StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/SampleActivity.java
@@ -0,0 +1,12 @@
+package com.samples.blackknight7.staticlistdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
