011f6f51b9b8bb1b7644c13a8f50eaf3c5608426
BlackKnight7 <blackknight7.h@gmail.com>
2016-01-22 20:27:46 +0800

    add custion action bar.

--- StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/SampleActivity.java
+++ StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/SampleActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class SampleActivity extends AppCompatActivity {
@@ -17,10 +19,21 @@ public class SampleActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.action_share:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
         }
