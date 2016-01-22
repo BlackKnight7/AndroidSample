118344b72ad0766a52b3b532333d7e0b7cc5cc10
BlackKnight7 <blackknight7.h@gmail.com>
2016-01-22 20:10:36 +0800

    add sdk support version and add action bar for navigation.

--- StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/MainActivity.java
+++ StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/MainActivity.java
@@ -1,27 +1,30 @@
 package com.samples.blackknight7.staticlistdemo;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     ListView listView;
     Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.list);
 
         // Defined Array values to show in ListView
-        String[] values = new String[]{"Android List View",
+        String[] values = new String[]{
+                "Android List View",
                 "Adapter implementation",
                 "Simple List View In Android",
                 "Create List View Android",
--- StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/SampleActivity.java
+++ StaticListDemo/app/src/main/java/com/samples/blackknight7/staticlistdemo/SampleActivity.java
@@ -1,12 +1,30 @@
 package com.samples.blackknight7.staticlistdemo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class SampleActivity extends Activity {
+public class SampleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
